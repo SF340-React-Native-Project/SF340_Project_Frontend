343b55388892a560f4e318ca3e9fb103d1109f20
Peerapat Sawettraporn <macnum4368@gmail.com>
2022-02-03 10:50:37 +0700

    home

--- screens/Home.js
+++ screens/Home.js
@@ -1,102 +1,123 @@
 import React, {useState} from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard, ScrollView } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Keyboard,
+  ScrollView,
+} from 'react-native';
 
 import Name from '../components/Name';
-import Food from "./Food";
+import Food from './Food';
 
-const Home = ({ navigation }) => {
-    const [name, setName] = useState();
-    const [nameItems, setNameItems] = useState([]);
+const Home = ({navigation}) => {
+  const [name, setName] = useState();
+  const [nameItems, setNameItems] = useState([]);
 
-    const dataPrice = [];
+  const dataPrice = [];
 
-    const handleAddTask = () => {
-        Keyboard.dismiss();
-        setNameItems([...nameItems, name])
-        setName(null);
-    }
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setNameItems([...nameItems, name]);
+    setName(null);
+  };
 
-    const completeTask = (index) => {
-        let itemsCopy = [...nameItems];
-        itemsCopy.splice(index, 1);
-        setNameItems(itemsCopy)
-    }
+  const completeTask = index => {
+    let itemsCopy = [...nameItems];
+    itemsCopy.splice(index, 1);
+    setNameItems(itemsCopy);
+  };
 
-    return (
+  const [toggle, settoggle] = useState(true);
+
+  return (
     <View style={styles.container}>
       {/* Added this scroll view to enable scrolling when list gets longer than the page */}
       <ScrollView
-          contentContainerStyle={{
-          flexGrow: 1
-          }}
-          keyboardShouldPersistTaps='handled'
-      >
-
+        contentContainerStyle={{
+          flexGrow: 1,
+        }}
+        keyboardShouldPersistTaps="handled">
         {/* Today's Tasks */}
         <View style={styles.tasksWrapper}>
-            <Text style={styles.sectionTitle}>Member</Text>
-            <View style={styles.items}>
+          <Text style={styles.sectionTitle}>Member</Text>
+          <Text style={styles.nameText}>Name</Text>
+          <View style={styles.items}>
             {/* This is where the tasks will go! */}
-            {
-              nameItems.map((item, index) => {
+            {nameItems.map((item, index) => {
               return (
-                <TouchableOpacity key={index}  onPress={() => completeTask(index)}>
-                    <Name text={item} /> 
+                <TouchableOpacity
+                  key={index}
+                  onPress={() => completeTask(index)}>
+                  <Name text={item} />
                 </TouchableOpacity>
-              )
-              })
-            }
-            </View>
+              );
+            })}
+            
+            {toggle? <TouchableOpacity style={styles.toggleButton} onPress={() => settoggle(toggle ? false : true)}>
+              <Text style={styles.toggleAdd}> Add Member</Text>
+            </TouchableOpacity> :<KeyboardAvoidingView
+              behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+              style={styles.writeTaskWrapper}>
+              <TextInput
+                style={styles.input}
+                placeholder={' Enter name'}
+                value={name}
+                onChangeText={text => setName(text)}
+              />
+              <TouchableOpacity onPress={() => handleAddTask()}>
+                <View style={styles.addWrapper}>
+                  <Text style={styles.addText}>+</Text>
+                </View>
+              </TouchableOpacity>
+            </KeyboardAvoidingView>}
+            
+            
+
+            
+          </View>
         </View>
-          
       </ScrollView>
 
       {/* Write a task */}
       {/* Uses a keyboard avoiding view which ensures the keyboard does not cover the items on screen */}
-      <KeyboardAvoidingView 
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-        style={styles.writeTaskWrapper}
-      >
-        <TextInput style={styles.input} placeholder={' Enter name'} value={name} onChangeText={text => setName(text)} />
-        <TouchableOpacity onPress={() => handleAddTask()}>
-            <View style={styles.addWrapper}>
-                <Text style={styles.addText}>+</Text>
-            </View>
-        </TouchableOpacity>
-      </KeyboardAvoidingView>
 
-      <TouchableOpacity onPress={() => { navigation.navigate('Food', { name: nameItems, dataPrice: dataPrice }); }} >
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate('Food', {name: nameItems, dataPrice: dataPrice});
+        }}>
         <Text style={{paddingLeft: 340}}> Next </Text>
       </TouchableOpacity>
-      
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#E8EAED',
+    backgroundColor: 'rgb(15,35,45)',
   },
   tasksWrapper: {
     paddingTop: 15,
     paddingHorizontal: 20,
   },
   sectionTitle: {
-    fontSize: 24,
-    fontWeight: 'bold'
+    fontSize: 30,
+    fontWeight: 'bold',
+    color : 'rgb(214,28,255)',
   },
   items: {
     marginTop: 30,
   },
   writeTaskWrapper: {
-    position: 'absolute',
-    bottom: 60,
     width: '100%',
     flexDirection: 'row',
     justifyContent: 'space-around',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   input: {
     paddingVertical: 15,
@@ -110,14 +131,44 @@ const styles = StyleSheet.create({
   addWrapper: {
     width: 60,
     height: 60,
-    backgroundColor: '#FFF',
+    color: '#0CF4FF',
     borderRadius: 60,
     justifyContent: 'center',
     alignItems: 'center',
-    borderColor: '#C0C0C0',
-    borderWidth: 1,
+    borderColor: '#A540FF',
+    borderWidth: 5,
   },
-  addText: {},
+  addText: {
+    fontSize: 35,
+    color: '#10FF92',
+  },
+  toggleAdd:{
+    borderRadius: 5,
+    borderColor: '#FF3CBE',
+    borderWidth: 1,
+    color : '#FF3CBE',
+    textAlign: 'center',
+    width: 300,
+    height: 70,
+    fontSize : 45,
+    fontFamily: 'Neonderthaw-Regular',
+    shadowColor: "#A540FF",
+            shadowOffset: {
+                width: 0,
+                height: 12,
+            },
+            shadowOpacity: 5,
+            shadowRadius: 16.00,
+            elevation: 50,  
+  },
+  toggleButton:{
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  nameText:{
+    color : '#F7FA13',
+    fontSize:20,
+  }
 });
 
 export default Home;
